82ad12f97c7e814d94f0bae780f7dd39601d45e5
Chen George <hengorg2019@gmail.com>
2025-05-20 01:18:25 +0300

    feat: Add new interactive CLI script for key generation and crypto operations

---
+++ python/cipher/encryption_decryption_cli.py
@@ -0,0 +1,62 @@
+import  random_substitution_cipher
+
+def intract_with_user():
+    enc_and_dec_keys = create_enc_and_dec_keys()
+    
+    print_create_enc_and_dec()
+    print_description_to_user()
+
+    while True:
+        user_command = command_from_user()
+        if user_command == "q":
+            break
+        elif user_command not in ['enc', 'dec']:
+            print_description_to_user()
+        else:
+            if user_command == 'enc':
+                # do function encrypted text
+                user_encrypted_text = encrypt_user_text(enc_and_dec_keys['enc_keys'])
+                print(f'Encrypted Text: "{user_encrypted_text}"')
+            else:
+                # do function decrypted text 
+                user_decrypted_text = encrypt_user_text(enc_and_dec_keys['dec_keys'])
+                print(f'Deccrypted Text: "{user_decrypted_text}"')
+                
+def create_enc_and_dec_keys():
+    encryped_keys = random_substitution_cipher.make_encryption_key()
+    decrypted_keys = random_substitution_cipher.compute_dec_key(encryped_keys)
+    encpyted_and_decypted_keys = {"dec_keys":decrypted_keys, "enc_keys": encryped_keys}
+    return(encpyted_and_decypted_keys)
+
+def print_create_enc_and_dec():
+    enc_and_dec_keys = create_enc_and_dec_keys()
+    print('---------------------')
+    print('Create Encrypted Keys:\n',enc_and_dec_keys['enc_keys'])
+    print('Create Decrypted Keys:\n',enc_and_dec_keys['dec_keys'])
+    print('---------------------')
+
+def print_description_to_user():
+    print('''
+"enc" For Encrypt Text
+"dec" For Decrypt Text
+"q"   For Exit''')
+    
+def encrypt_user_text(encryped_keys):
+    user_command_enc = input('Enter text to encrypt> ') 
+    user_encrypted_text = random_substitution_cipher.encrypt_text(user_command_enc, encryped_keys)
+    return user_encrypted_text
+
+def decrypt_user_text(decryped_keys):
+    user_command_enc = input('Enter text to decrypt> ') 
+    user_encrypted_text = random_substitution_cipher.encrypt_text(user_command_enc, decryped_keys)
+    return user_encrypted_text
+
+def command_from_user():
+    while True:
+        user_input = input('> ')
+        return user_input
+        
+intract_with_user()
+
+if __name__ == "__main__":
+    intract_with_user()
