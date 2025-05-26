e84ce7a1204cd463a9f0b4be2a80e0e6413cc0be
Chen George <hengorg2019@gmail.com>
2025-05-26 11:26:54 +0300

    feat: Modularize encryption & add JSON save
    Refactored encryption into smaller, action-specific functions for better code organization. Added an option to save the encrypted output to a JSON file.

--- python/cipher/encryption_decryption_cli.py
+++ python/cipher/encryption_decryption_cli.py
@@ -1,4 +1,5 @@
 import  random_substitution_cipher
+import json
 
 def intract_with_user():
     enc_and_dec_keys = create_enc_and_dec_keys()
@@ -40,26 +41,43 @@ def decrypt_user_text(user_command_dec,decryped_keys):
     user_encrypted_text = random_substitution_cipher.encrypt_text(user_command_dec, decryped_keys)
     return user_encrypted_text
 
-def return_enc_and_dec_by_user_choise(user_command, enc_and_dec_keys):
-    if user_command == 'enc':
-        # do function encrypted text
-        user_command_enc = input('Enter text to encrypt> ') 
-        user_encrypted_text = encrypt_user_text(user_command_enc,enc_and_dec_keys['enc_keys'])
-        print(f'Encrypted Text: "{user_encrypted_text}"')
-        return user_encrypted_text
-    else:
+def save_encrypted_text_to_file(user_encrypted_text):
+    encrypted_text_file = open("encrypted_text.json", 'w')
+    json.dump(user_encrypted_text, encrypted_text_file)
+    encrypted_text_file.close()
+    
+def handle_user_enc_choise(enc_and_dec_keys):
+    
+    user_command_enc = input('Enter text to encrypt> ') 
+    user_encrypted_text = encrypt_user_text(user_command_enc,enc_and_dec_keys['enc_keys'])
+    
+    user_command_enc = input('Do you want to save the encrypted text to a file? (y/n)> ') 
+    if user_command_enc == 'y':
+        print('Saving encrypted text "encrypted_text.json" to a file...')
+        save_encrypted_text_to_file(user_encrypted_text)
+    
+    return user_encrypted_text
+
+def handle_user_dec_choise(enc_and_dec_keys):
         # do function decrypted text 
         user_command_dec = input('Enter text to decrypt> ') 
         user_decrypted_text = decrypt_user_text(user_command_dec,enc_and_dec_keys['dec_keys'])
-        print(f'Deccrypted Text: "{user_decrypted_text}"')
         return user_decrypted_text
-    
 
+def return_enc_and_dec_by_user_choise(user_command, enc_and_dec_keys):
+    if user_command == 'enc':
+        user_encrypted_text = handle_user_enc_choise(enc_and_dec_keys)
+        print(f'Encrypted Text: "{user_encrypted_text}"')
+        return user_encrypted_text
+    else:
+        if user_command == 'dec':
+            dec_choise = handle_user_dec_choise(enc_and_dec_keys)
+            print(f'Deccrypted Text: "{dec_choise}"')
+            return dec_choise 
 
 def handle_command_from_user(enc_and_dec_keys):
     while True:
         user_command = input('> ')
-        print_description_to_user()
         if user_command == "q":
             return user_command
         # quit_if_q_letter(user_command)  
